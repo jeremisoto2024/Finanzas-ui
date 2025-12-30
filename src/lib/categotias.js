522873ea4d2471b9ccc43a84fb660152dc7c83e8
jeremisoto2024 <jeremisoto2024@gmail.com>
2025-12-30 14:44:52 +0100

    Crear categotias.js

---
+++ src/lib/categotias.js
@@ -0,0 +1,25 @@
+import {
+  ShoppingCartIcon,
+  HomeIcon,
+  TruckIcon,
+  EllipsisHorizontalIcon
+} from '@heroicons/react/24/outline'
+
+export const categoriasConfig = {
+  Alimentación: {
+    icon: ShoppingCartIcon,
+    color: 'text-emerald-400'
+  },
+  Alquiler: {
+    icon: HomeIcon,
+    color: 'text-indigo-400'
+  },
+  Transporte: {
+    icon: TruckIcon,
+    color: 'text-yellow-400'
+  },
+  Otros: {
+    icon: EllipsisHorizontalIcon,
+    color: 'text-slate-400'
+  }
+}
