0ccc3bfd8435bb99e7d281a9d49d8f655d0e7fcd
jeremisoto2024 <jeremisoto2024@gmail.com>
2025-12-30 14:43:37 +0100

    Crear data.js

---
+++ src/lib/data.js
@@ -0,0 +1,6 @@
+export const gastos = [
+  { categoria: 'Alimentación', monto: 120 },
+  { categoria: 'Alquiler', monto: 425 },
+  { categoria: 'Transporte', monto: 38 },
+  { categoria: 'Otros', monto: 22 }
+]
