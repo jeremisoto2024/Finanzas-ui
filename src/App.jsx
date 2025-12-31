1168b0b0b415b2f218461630030617138cc07fe1
jeremisoto2024 <jeremisoto2024@gmail.com>
2025-12-31 02:16:10 +0100

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
-        className="max-w-md space-y-4 mx-auto"
+        className="max-w-md space-y-6 mx-auto"
       >
         <ResumenGastos />
 
