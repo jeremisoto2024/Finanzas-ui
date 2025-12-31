1e97613b395fbbd38a1cd4c3542adc4cbc0241ef
jeremisoto2024 <jeremisoto2024@gmail.com>
2025-12-31 10:03:46 +0100

    Actualizar categorias.js

--- src/lib/categorias.js
+++ src/lib/categorias.js
@@ -8,18 +8,22 @@ import {
 export const categoriasConfig = {
   Alimentación: {
     icon: ShoppingCartIcon,
-    color: 'text-emerald-400'
+    color: 'text-emerald-400',
+    hex: '#34d399'
   },
   Alquiler: {
     icon: HomeIcon,
-    color: 'text-indigo-400'
+    color: 'text-indigo-400',
+    hex: '#818cf8'
   },
   Transporte: {
     icon: TruckIcon,
-    color: 'text-yellow-400'
+    color: 'text-yellow-400',
+    hex: '#facc15'
   },
   Otros: {
     icon: EllipsisHorizontalIcon,
-    color: 'text-slate-400'
+    color: 'text-slate-400',
+    hex: '#94a3b8'
   }
 }
