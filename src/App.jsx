c686bc5b50757d2f7ff791e62c9087a3c6a70163
jeremisoto2024 <jeremisoto2024@gmail.com>
2025-12-31 10:42:45 +0100

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 import Layout from "./components/layout/Layout"
 import ResumenGastos from "./components/dashboard/ResumenGastos"
-import GraficoGastos from '@/components/dashboard/GraficoGastos'
+import GraficoGastos from "./components/dashboard/GraficoGastos'
 import { Button } from "./components/ui/button"
 
 export default function App() {
