008260ab579a514069b8794a9785b379765ef2c3
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-01 03:00:07 +0100

    Actualizar GraficoGastos.jsx

--- src/components/dashboard/GraficoGastos.jsx
+++ src/components/dashboard/GraficoGastos.jsx
@@ -30,7 +30,7 @@ export default function GraficoGastos() {
         </CardTitle>
       </CardHeader>
 
-      <CardContent <div className="h-64 w-full">
+      <CardContent <div className="h-64 w-full">>
           <ResponsiveContainer width="100%" height="100%">
             <motion.div
               initial={{ opacity: 0, scale: 0.9 }}
