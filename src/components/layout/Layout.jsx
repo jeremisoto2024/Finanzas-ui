fe23555a411bed7af6986d993097f7105fc4a89d
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-05 00:24:04 +0100

    Actualizar Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 
-export default function Layout({ children }) {
+  export default function Layout({ children }) {
+  const [menuAbierto, setMenuAbierto] = useState(false)
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100 md:flex">
-      <Sidebar />
+      <div className="hidden md:block">
+  <Sidebar />
+</div>
 
       <div className="flex-1">
-        <Navbar />
+        <Navbar onMenu={() => setMenuAbierto(true)} />
+        {menuAbierto && (
+  <div className="fixed inset-0 z-50 md:hidden">
+    {/* Fondo oscuro */}
+    <div
+      className="absolute inset-0 bg-black/50"
+      onClick={() => setMenuAbierto(false)}
+    />
+
+    {/* Sidebar */}
+    <div className="relative h-full w-64 bg-slate-950 border-r border-slate-800">
+      <Sidebar />
+    </div>
+  </div>
+)}
 
         <main className="p-4 md:p-6">
           {children}
