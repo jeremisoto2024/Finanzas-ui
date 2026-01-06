59bdbca49e31a4ebac1686001be46760138f389d
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-06 12:08:36 +0100

    Crear gastos.js

---
+++ src/lib/gastos.js
@@ -0,0 +1,23 @@
+export const gastosMensuales = [
+  {
+    id: 1,
+    fecha: '2025-10-02',
+    concepto: 'Alquiler',
+    categoria: 'Alquiler',
+    monto: 750
+  },
+  {
+    id: 2,
+    fecha: '2025-10-05',
+    concepto: 'Supermercado',
+    categoria: 'Alimentación',
+    monto: 120.35
+  },
+  {
+    id: 3,
+    fecha: '2025-10-08',
+    concepto: 'Netflix',
+    categoria: 'Otros',
+    monto: 12
+  }
+]
