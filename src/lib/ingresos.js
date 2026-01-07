ff8ae3b8a1cd53aad6ddfba9e8d478798c7655c4
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-07 20:13:10 +0100

    Crear src/lib/ingresos.js

---
+++ src/lib/ingresos.js
@@ -0,0 +1,20 @@
+export const ingresosMensuales = [
+  {
+    id: 1,
+    fecha: '2025-10-01',
+    concepto: 'Salario',
+    categoria: 'Trabajo',
+    monto: 1800,
+    metodo: 'Transferencia',
+    cuenta: 'BBVA'
+  },
+  {
+    id: 2,
+    fecha: '2025-10-10',
+    concepto: 'Freelance',
+    categoria: 'Extra',
+    monto: 350,
+    metodo: 'Transferencia',
+    cuenta: 'Revolut'
+  }
+]
