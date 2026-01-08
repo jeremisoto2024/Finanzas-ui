3d1405e856f50d52bde68233c57b2598f59214f1
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-08 03:18:06 +0100

    Actualizar Ingresos.jsx

--- src/pages/Ingresos.jsx
+++ src/pages/Ingresos.jsx
@@ -1,10 +1,6 @@
+// src/app/ingresos/page.jsx
 import TablaIngresos from '@/components/ingresos/TablaIngresos'
 
-export default function Ingresos() {
-  return (
-    <div className="space-y-6">
-      <h1 className="text-xl font-semibold">Ingresos del mes</h1>
-      <TablaIngresos />
-    </div>
-  )
+export default function IngresosPage() {
+  return <TablaIngresos />
 }
