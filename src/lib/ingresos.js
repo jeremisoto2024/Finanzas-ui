ae67e2d6534f10e3bb8ef6e692c2198a0f2ec143
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-08 03:02:43 +0100

    Actualizar ingresos.js

--- src/lib/ingresos.js
+++ src/lib/ingresos.js
@@ -2,8 +2,8 @@ export const ingresosMensuales = [
   {
     id: 1,
     fecha: '2025-10-01',
-    concepto: 'Salario',
-    categoria: 'Trabajo',
+    concepto: 'Salario mensual',
+    categoria: 'Sueldo',
     monto: 1800,
     metodo: 'Transferencia',
     cuenta: 'BBVA'
@@ -11,10 +11,82 @@ export const ingresosMensuales = [
   {
     id: 2,
     fecha: '2025-10-10',
-    concepto: 'Freelance',
-    categoria: 'Extra',
+    concepto: 'Proyecto freelance',
+    categoria: 'Freelance',
     monto: 350,
     metodo: 'Transferencia',
     cuenta: 'Revolut'
+  },
+  {
+    id: 3,
+    fecha: '2025-10-15',
+    concepto: 'Propina restaurante',
+    categoria: 'Propina',
+    monto: 25,
+    metodo: 'Efectivo',
+    cuenta: 'Efectivo'
+  },
+  {
+    id: 4,
+    fecha: '2025-10-20',
+    concepto: 'Venta de muebles',
+    categoria: 'Ventas',
+    monto: 120,
+    metodo: 'Bizum',
+    cuenta: 'BBVA'
+  },
+  {
+    id: 5,
+    fecha: '2025-10-25',
+    concepto: 'Bonos de productividad',
+    categoria: 'Extras',
+    monto: 150,
+    metodo: 'Transferencia',
+    cuenta: 'BBVA'
+  },
+  {
+    id: 6,
+    fecha: '2025-09-01',
+    concepto: 'Salario mensual',
+    categoria: 'Sueldo',
+    monto: 1800,
+    metodo: 'Transferencia',
+    cuenta: 'BBVA'
+  },
+  {
+    id: 7,
+    fecha: '2025-09-15',
+    concepto: 'Trabajo freelance',
+    categoria: 'Freelance',
+    monto: 450,
+    metodo: 'PayPal',
+    cuenta: 'PayPal'
+  },
+  {
+    id: 8,
+    fecha: '2025-10-07',
+    concepto: 'ABRIR',
+    categoria: 'Propina',
+    monto: 1.04,
+    metodo: 'Efectivo',
+    cuenta: 'BBVA'
+  },
+  {
+    id: 9,
+    fecha: '2025-10-06',
+    concepto: 'ABRIR',
+    categoria: 'Propina',
+    monto: 3.82,
+    metodo: 'Efectivo',
+    cuenta: 'Efectivo'
+  },
+  {
+    id: 10,
+    fecha: '2025-10-05',
+    concepto: 'ABRIR',
+    categoria: 'Propina',
+    monto: 3.50,
+    metodo: 'Efectivo',
+    cuenta: 'Efectivo'
   }
-]
+];
