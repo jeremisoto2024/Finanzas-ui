df83483082ea3b49a49c82fcdab9db49c21d51dd
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-10 02:31:52 +0100

    Actualizar TablaIngresos.jsx

--- src/components/ingresos/TablaIngresos.jsx
+++ src/components/ingresos/TablaIngresos.jsx
@@ -849,15 +849,3 @@ export default function TablaIngresos() {
     </div>
   );
 }
-
-// Componente PieChart
-function PieChart({ className }) {
-  return (
-    <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
-        d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
-        d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
-    </svg>
-  );
-}
