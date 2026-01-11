9c1c5b3fc861962111ca2af348ee3abd3fedaa28
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-11 11:27:07 +0100

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Layout from './components/layout/Layout'
+import { GastosProvider } from '@/contexts/GastosContext' // Importar el provider
 
 import Dashboard from './pages/Dashboard'
 import Gastos from './pages/Gastos'
@@ -9,10 +10,13 @@ export default function App() {
   const [activePage, setActivePage] = useState('Dashboard')
 
   return (
-    <Layout>
-      {activePage === 'Dashboard' && <Dashboard />}
-      {activePage === 'Gastos' && <Gastos />}
-      {activePage === 'Configuración' && <Configuracion />}
-    </Layout>
+    // Envolver toda la aplicación con GastosProvider
+    <GastosProvider>
+      <Layout>
+        {activePage === 'Dashboard' && <Dashboard />}
+        {activePage === 'Gastos' && <Gastos />}
+        {activePage === 'Configuración' && <Configuracion />}
+      </Layout>
+    </GastosProvider>
   )
 }
