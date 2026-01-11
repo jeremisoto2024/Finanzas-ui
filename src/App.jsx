2b3de7eb6821d378050b87ecc1fce8ea5beca59d
jeremisoto2024 <jeremisoto2024@gmail.com>
2026-01-11 10:55:46 +0100

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Layout from './components/layout/Layout'
+
 import Dashboard from './pages/Dashboard'
 import Gastos from './pages/Gastos'
 import Configuracion from './pages/Configuracion'
@@ -8,7 +9,7 @@ export default function App() {
   const [activePage, setActivePage] = useState('Dashboard')
 
   return (
-    <Layout activePage={activePage} setActivePage={setActivePage}>
+    <Layout>
       {activePage === 'Dashboard' && <Dashboard />}
       {activePage === 'Gastos' && <Gastos />}
       {activePage === 'Configuración' && <Configuracion />}
